7696d7e180ef0598049dbcd751c04bd98886af84
Yanik Perez <ypmsgf@gmail.com>
2022-10-03 15:51:28 +0200

    Ein bischen zu viel (Sorry)

---
+++ 1.8/solution/hihi.js
@@ -0,0 +1,13 @@
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 37) {
+        alert('Left was pressed');
+
+    }
+    else if (event.keyCode == 39) {
+        alert('Right was pressed');
+    }else if (event.keyCode == 38) {
+        alert('Up was pressed');
+    }else if (event.keyCode == 40) {
+        alert('Down was pressed');
+    }
+}, true);
